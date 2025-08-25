4e74457c886a273512f3bd762876197d1cb906f5
leo <leonardo8571@gmail.com>
2025-08-25 17:15:57 -0300

    DONE

--- ead/atividade JS 1/script.js
+++ ead/atividade JS 1/script.js
@@ -8,7 +8,7 @@ function e1 () {
         document.write("<h4>" + tempF + " não é uma temperature válida</h212>");
     }
 
-    document.write("<table>")
+    document.write("<table class='temp'>")
     
     document.write("<tr>")
     document.write("<td>Temperatura em Celsius</td>")
@@ -30,25 +30,59 @@ function e1 () {
     document.write("<td class='quente'>Quente</td>")
     document.write("</tr>")
 
-
     document.write("</table>")
-
-
-
 }
 
 let e2 = function () {
     let arrayNotas = [];
-    for (let i = 0; i <= 4; i++){
-        arrayNotas[i] = prompt(`Informe a ${i}ª nota`);
+    for (let i = 0; i < 4; i++){
+        arrayNotas[i] = prompt(`Informe a ${1+i}ª nota.`);
     }
 
     return arrayNotas;
 }
 
 let e3 = () => {
-    notas = e3();
+    let notas = e2();
+    let somaNotas = 0;    
+
+    document.write("<table class='notas'>");
+    document.write("<tr>");
+    document.write("<th>Nota1</th>");
+    document.write("<th>Nota2</th>");
+    document.write("<th>Nota3</th>");
+    document.write("<th>Nota4</th>");
+    document.write("<th>Média</th>");
+    document.write("<th>Resultado</th>");
+    document.write("<th>Imagem</th>");
+    document.write("</tr>");
+    
+    document.write("<tr>");
+    notas.forEach (i => {
+        somaNotas += parseFloat(i);
+        document.write(`<td>${i}</td>`);
+    })
+    let media = somaNotas / notas.length;
+    document.write(`<td>${media}</td>`);
+    
+    if (media >= 7) {
+        res = "Aprovado";
+        img = "https://cdn-icons-png.flaticon.com/512/4157/4157080.png";
+    } else if (media >= 1.8 ) {
+        res = "Em Exame";
+        img = "https://cdn-icons-png.flaticon.com/512/2299/2299283.png";
+    } else {
+        res = "Reprovado";
+        img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFcgQrsfeYiodqNrrLGtTpaVEkNnfswjh1oQ&s";
+    }
+    
+    document.write(`<td>${res}</td>`);
+    document.write(`<td><img src="${img}"></img></td>`);
+    
+    document.write("</tr>");
+    document.write("</table>");
 }
 
 
 e1();
+e3();
