6985e2bb3b217dc039a6bd5449ad4f40351099e4
Leonardo Rodrigues da Silva <leo@mac.local>
2025-10-06 00:28:15 -0300

    Cleanup

--- presencial/aula1002/vjQuery/script.js
+++ presencial/aula1002/vjQuery/script.js
@@ -1,60 +1,46 @@
 $(document).ready(() => {
     let btn = $("#calcular");
-    $("#concordo").on("click", () => btn.prop("disabled", !btn.prop("disabled")));
-    
-    $('#resultado').hide();
-    $(".inp").on("keyup change", () => $("#resultado").hide() );
+    $("#concordo").click( () => btn.prop("disabled", !btn.prop("disabled")) );
 
-    $("#calcular").click(calcular_imc);
+    $('#resultado').hide();
+    $(".inp").on("keyup change", () => $("#resultado").hide());
+
+    btn.click((calcular_imc));
 });
 
-function validaNumero(elemento) {
-    return $(elemento).val().match(/^[\d.]+$/) ? true : false;
-}
-
 function validaCampos() {
-    let res = true;
+    let tudoValido = true;
 
     let camposNum = $("input[type=number]");
     let generos = $("input[name='genero']");
-    
+
     temGenero = generos.is(":checked");
     generos.toggleClass("is-invalid", !temGenero);
-    
-    camposNum.each(function () {
-        let valido = validaNumero(this);
-        $(this).toggleClass('is-invalid', !valido);
-        res = res & valido;
-    })
 
-    return res && temGenero;
+    camposNum.each(function () {
+        let numValido = /^[\d.]+$/.test( $(this).val() );
+        $(this).toggleClass('is-invalid', !numValido);
+        tudoValido = tudoValido && numValido;
+    });
+
+    return tudoValido && temGenero;
 }
 
 function calcular_imc() {
-    if (validaCampos()) {
-        let altura = $("#altura").val();
-        let peso = $("#peso").val();
-
-        let imc = peso / (altura * altura);
-        mostrar_resultado(imc);
-    }
+    if (!validaCampos()) return;
+    let imc = $("#peso").val() / ($("#altura").val() ** 2);
+    mostrar_resultado(imc);
 }
 
 function mostrar_resultado(valor) {
-    let masc = $('#m').prop("checked");
-    let img = $("#img");
-    let imc_desc = $("#imc_desc");
+    let genero = $('#m').prop("checked") ? "m" : "f";
+    let resultado;
 
-    if (valor > 28) {
-        img.attr( "src", masc ? "imgs/m3.png" : "imgs/f3.png");
-        imc_desc.text("IMC = " + valor + " - Obesidade");
-    } else if (valor >= 18) {
-        img.attr( "src", masc ? "imgs/m2.png" : "imgs/f2.png");
-        imc_desc.text("IMC = " + valor + " - Sobrepeso");
-    } else {
-        img.attr( "src", masc ? "imgs/m1.png" : "imgs/f1.png");
-        imc_desc.text("IMC = " + valor + " - Normal");
-    }
+    if      (valor > 28) resultado = "Obesidade";
+    else if (valor > 18) resultado = "Sobrepeso";
+    else                 resultado = "Normal";
 
+    $("#imc_desc").text(`IMC = ${valor} - ${resultado}`);
+    $("#img").attr("src", `imgs/${genero}_${resultado}.png`);
     $("#resultado").show();
 }
