035854dc1ea30fa30d45ec747cf04da0f8cb8134
Leonardo Rodrigues da Silva <leo@mac.local>
2025-10-09 22:43:21 -0300

    primeira prova de Web II

---
+++ 1 prova web 2/js/script.js
@@ -0,0 +1,49 @@
+$(window).ready(() => {
+    // Query dos elementos com jQuery
+    let img = $('#img');
+    let msg = $("#msg");
+    let qtd = $("#qtd");
+    let valor = $("#valor");
+    let previsao = $("#previsao");
+    let btn_comprar = $("#comprar");
+    let cor_select = $('#cor-select');
+
+    // Pega a data de hoje, calcula 12 dias no futuro e mostra
+    let hoje = new Date();
+    let entrega = new Date(hoje.getTime() + 86400000*12);
+    previsao.text(`Previsão de Entrega: ${entrega.getDate()}/${entrega.getMonth() + 1}/${entrega.getFullYear()}`)
+
+    // Esconde a mensagem que aparece quando clica em Comprar
+    msg.hide();
+
+    // Muda a imagem quando a cor é selecionada
+    cor_select.change(() => img.attr("src", "imgs/" + cor_select.val() + ".webp"));
+
+    // Muda o valor de um input com base em outro input
+    // A quantidade é convertida um inteiro
+    // O valor total só aparece se o numero for valido
+    qtd.on("keyup change", () => {
+        let valor_total = Math.max( parseInt(qtd.val()) * 1109, 0);
+        if (valor_total && valor_total > 0) valor.val("R$ " + valor_total);
+        else valor.val("");
+    }
+    );
+
+    // Chama a funcao comprar quando o botao é clickado
+    btn_comprar.click(comprar);
+
+    function comprar() {
+        // se a quantidade for valida
+        if (qtd.val() > 0) {
+            msg.text("Compra efetuada com sucesso! ");
+            msg.css("color", "green");
+        }
+        // Se nao for valida
+        else { 
+            msg.text("Informe a quantidade!");
+            msg.css("color", "red");
+        };
+        // mostra a mesnsagem no final
+        msg.show();
+    }
+});
